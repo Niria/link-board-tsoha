9d2b26a810bd96e144fa81fd1de4665284dcf84f
Matias Rinne <147260381+Niria@users.noreply.github.com>
2024-11-06 12:41:00 +0200

    Create initial db schema and file structure for the project
    - Added requirements.txt for dependencies
    - Created first db schema for the project
    - Added a few basic templates
    - Added first routes to the app: index, register, login and logout
    - Registering and login/logout are somewhat functional
    - Added relevant files to gitignore

---
+++ schema.sql
@@ -0,0 +1,44 @@
+BEGIN;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    display_name TEXT,
+    password TEXT NOT NULL,
+    profile_public BOOLEAN DEFAULT TRUE,
+    user_role SMALLINT DEFAULT 0
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    content TEXT,
+    link_url TEXT NOT NULL,
+    visible BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE replies (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users NOT NULL,
+    thread_id INTEGER REFERENCES threads NOT NULL,
+    parent_id INTEGER REFERENCES replies,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    is_public BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE permissions (
+    user_id INTEGER REFERENCES users NOT NULL,
+    category_id INTEGER REFERENCES categories NOT NULL,
+    PRIMARY KEY (user_id, category_id),
+    can_read BOOLEAN DEFAULT TRUE,
+    can_write BOOLEAN DEFAULT TRUE
+);
+
+COMMIT;
