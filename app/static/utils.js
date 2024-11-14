9bc1f2203a0c650882e8831e89a5acc43dbd3b34
Matias Rinne <147260381+Niria@users.noreply.github.com>
2024-11-14 17:21:13 +0200

    Refactor js toggle and like scripts to use eventListeners
    - The scripts are functional, but there might still be some cleaning
    and restructuring that needs to be done

--- app/static/utils.js
+++ app/static/utils.js
@@ -1,8 +1,3 @@
-function toggleCommentForm(event) {
-  const form = document.getElementById(`toggle-comment-form-${event.target.dataset.replyform}`);
-  form.classList.toggle('hidden')
-}
-
 function likeThread() {
   const url = document.getElementById('like-thread').dataset.url;
   const csrf_token = JSON.parse(document.getElementById('like-thread').dataset.csrf);
@@ -20,10 +15,10 @@ function likeThread() {
     .then((json) => document.getElementById('thread-likes').innerHTML = `${json.likes} likes`);
 }
 
-function likeReply(reply_id) {
-  const url = document.getElementById(`like-reply-${reply_id}`).dataset.url;
-  const csrf_token = JSON.parse(document.getElementById(`like-reply-${reply_id}`).dataset.csrf);
-  const image = document.getElementById(`thumb-${reply_id}`);
+function likeReply(reply) {
+  const url = reply.dataset.url;
+  const csrf_token = JSON.parse(reply.dataset.csrf);
+  const image = reply.querySelector('#thumb');
   image.classList.toggle('thumbactive');
   fetch(url, {
     method: "POST",
@@ -33,5 +28,34 @@ function likeReply(reply_id) {
     }
   })
     .then((response) => response.json())
-    .then((json) => document.getElementById(`reply-likes-${reply_id}`).innerHTML = `${json.likes} likes`);
+    .then((json) => reply.querySelector('#reply-likes').innerHTML = `${json.likes} likes`);
+}
+
+function setEventHandlers() {
+  // Initialize eventListener for thread clicks
+  const threadContent = document.querySelector('.thread-content')
+  if ( threadContent !== null) {
+    const threadComment = threadContent.querySelector('.toggle-comment-form')
+    threadContent.querySelector('.form-toggle').addEventListener('click', () => {
+      threadComment.classList.toggle('hidden')
+    })
+    threadContent.querySelector('#like-thread').addEventListener('click', likeThread)
   }
+
+  // Initialize eventlisteners for replies clicks
+  const replyContent = document.querySelectorAll('.reply-content')
+  if (replyContent !== null) {
+    replyContent.forEach((reply) => {
+      const replyComment = reply.querySelector('.toggle-comment-form');
+      if (replyComment !== null) {
+        reply.querySelector('.form-toggle').addEventListener('click', () => {
+          replyComment.classList.toggle('hidden')
+        })
+      }
+      const replyLike = reply.querySelector('#like-reply')
+      replyLike.addEventListener('click', () => likeReply(reply))
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => setEventHandlers())
